6e005cd9dfbc9309aaaffc34d0f3cbab7f0eccd2
Parham Alvani <parham.alvani@gmail.com>
2016-05-18 20:22:39 +0430

    Implement crossover method.

--- kromosom.go
+++ kromosom.go
@@ -28,3 +28,54 @@ func (k *Kromosom) Mutate() {
 		k.Gen[a], k.Gen[b] = k.Gen[b], k.Gen[a]
 	}
 }
+
+func Crossover(k1 *Kromosom, k2 *Kromosom) (Kromosom, Kromosom) {
+	var d1 Kromosom
+	var d2 Kromosom
+
+	var d1i [16]bool
+	var d2i [16]bool
+
+	for i := 0; i < 16; i++ {
+		d1i[i] = false
+		d2i[i] = false
+	}
+
+	var alpha uint8
+	/* Get alpha from LFSR1 */
+	alpha = alpha & 0x0F
+
+	for i := 0; i < int(alpha); i++ {
+		d1.Gen[i] = k1.Gen[i]
+		d1i[k1.Gen[i]-1] = true
+
+		d2.Gen[i] = k2.Gen[i]
+		d2i[k2.Gen[i]-1] = true
+
+	}
+
+	var i1, i2 int
+	for i := alpha; i < 16; i++ {
+		flag1 := true
+		for ; i1 < 16 && flag1; i1++ {
+			index := k2.Gen[i1]
+			if d1i[index-1] == false {
+				d1.Gen[i] = index
+				d1i[index-1] = true
+				flag1 = false
+			}
+		}
+
+		flag2 := true
+		for ; i2 < 16 && flag2; i2++ {
+			index := k1.Gen[i2]
+			if d2i[index-1] == false {
+				d2.Gen[i] = index
+				d2i[index-1] = true
+				flag2 = false
+			}
+		}
+	}
+
+	return d1, d2
+}
