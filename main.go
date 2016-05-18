aec3442e6b62dfce374c9d4983e7592347672b2f
Parham Alvani <parham.alvani@gmail.com>
2016-05-18 18:33:32 +0430

    Complete serial read phase :)

--- main.go
+++ main.go
@@ -17,10 +17,11 @@ import (
 )
 
 func main() {
-	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
+	c := &serial.Config{Name: "/dev/tnt0", Baud: 9600}
 	c.Parity = serial.ParityNone
 	c.StopBits = serial.Stop1
 	c.Size = serial.DefaultSize
+
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
@@ -30,26 +31,32 @@ func main() {
 	buf := make([]byte, 128)
 
 	/* Read seed number 1 */
+	var s1 uint8
 	n, err = s.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	s1, err := strconv.Atoi(string(buf[:n]))
+	if n != 1 {
+		log.Fatal("We get more than one byte :(")
+	}
+	s1 = uint8(buf[0])
 	log.Printf("%d", s1)
 
 	/* Read seed number 2 */
+	var s2 uint8
 	n, err = s.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	s2, err := strconv.Atoi(string(buf[:n]))
+	s2 = uint8(buf[0])
 	log.Printf("%d", s2)
 
 	/* Read seed number 3 */
+	var s3 uint8
 	n, err = s.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	s3, err := strconv.Atoi(string(buf[:n]))
+	s3 = uint8(buf[0])
 	log.Printf("%d", s3)
 }
