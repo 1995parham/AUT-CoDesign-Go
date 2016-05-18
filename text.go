49e414fe070df68a084c427f3cfc7948fd184dd6
Parham Alvani <parham.alvani@gmail.com>
2016-05-18 21:15:57 +0430

    RefText variable was added.

---
+++ text.go
@@ -0,0 +1,12 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 18-05-2016
+ * |
+ * | File Name:     text.go
+ * +===============================================
+ */
+package main
+
+var RefText string
