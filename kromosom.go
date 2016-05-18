cfc299570fc2be8994fcd121bdb14942e619eab3
Parham Alvani <parham.alvani@gmail.com>
2016-05-18 19:28:48 +0430

    Add mutate function for kromosom.

--- kromosom.go
+++ kromosom.go
@@ -13,3 +13,18 @@ type Kromosom struct {
 	Gen     [16]int
 	Fitness uint64
 }
+
+func (k *Kromosom) Mutate() {
+	/* Get p from LFSR2 */
+	var p uint8
+
+	if p < 64 {
+		/* Get indicator from LFSR3 */
+		var indicator uint8
+
+		a := indicator & 0x0F
+		b := indicator >> 4
+
+		k.Gen[a], k.Gen[b] = k.Gen[b], k.Gen[a]
+	}
+}
