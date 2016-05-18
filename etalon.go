d0a065176fa3bf7573b79a230faa806544427d60
Parham Alvani <parham.alvani@gmail.com>
2016-05-18 20:40:45 +0430

    Etalon array was added.

---
+++ etalon.go
@@ -0,0 +1,12 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 18-05-2016
+ * |
+ * | File Name:     etalon.go
+ * +===============================================
+ */
+package main
+
+var E [27][27]uint64
