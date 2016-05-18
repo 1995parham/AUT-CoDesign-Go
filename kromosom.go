7af50f905a22e2960f488e9d3041408f5a8bc3e6
Parham Alvani <parham.alvani@gmail.com>
2016-05-18 18:56:24 +0430

    Add kromosom structure.

---
+++ kromosom.go
@@ -0,0 +1,15 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 18-05-2016
+ * |
+ * | File Name:     kromosom.go
+ * +===============================================
+ */
+package main
+
+type Kromosom struct {
+	gen     [16]int
+	fitness uint64
+}
